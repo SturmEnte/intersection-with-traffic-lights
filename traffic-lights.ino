db9b4124a06de0759f34406288ed5d14c5e953b8
SturmEnte <sturmente@proton.me>
2022-10-04 21:31:28 +0200

    Added an phase where all traffic lights are red

--- traffic-lights.ino
+++ traffic-lights.ino
@@ -15,23 +15,34 @@ const int LONG_PHASE = 9000;
 const int SHORT_PHASE = 1000;
 
 void setup() {
-	pinMode(RED1, OUTPUT);
-	pinMode(YELLOW1, OUTPUT);
-	pinMode(GREEN1, OUTPUT);
-	pinMode(RED2, OUTPUT);
-	pinMode(YELLOW2, OUTPUT);
-	pinMode(GREEN2, OUTPUT);
+  pinMode(RED1, OUTPUT);
+  pinMode(YELLOW1, OUTPUT);
+  pinMode(GREEN1, OUTPUT);
+  pinMode(RED2, OUTPUT);
+  pinMode(YELLOW2, OUTPUT);
+  pinMode(GREEN2, OUTPUT);
+  
+  // Ampel 1 Rot
+  setRed(1, HIGH);
 }
 
 void loop() {
+  // Ampel 2 Rot
+  setGreen(2, LOW);
+  setYellow(2, LOW);
+  setRed(2, HIGH);
+
+  delay(SHORT_PHASE);
+  
+  // Ampel 1 Rot, Gelb
+  setYellow(1, HIGH);
+
+  delay(SHORT_PHASE);
+  
   // Ampel 1 Grün
   setRed(1, LOW);
   setYellow(1, LOW);
   setGreen(1, HIGH);
-
-  // Ampel 2 Rot
-  setYellow(2, LOW);
-  setRed(2, HIGH);
 
   delay(LONG_PHASE);
 
@@ -39,14 +50,18 @@ void loop() {
   setGreen(1, LOW);
   setYellow(1, HIGH);
 
-  // Ampel 2 Rot, Gelb
-  setYellow(2, HIGH);
-
   delay(SHORT_PHASE);
 
   // Ampel 1 Rot
   setYellow(1, LOW);
   setRed(1, HIGH);
+
+  delay(SHORT_PHASE);
+
+  // Ampel 2 Rot, Gelb
+  setYellow(2, HIGH);
+
+  delay(SHORT_PHASE);
 
   // Ampel 2 Grün
   setRed(2, LOW);
@@ -54,9 +69,6 @@ void loop() {
   setGreen(2, HIGH);
 
   delay(LONG_PHASE);
-
-  // Ampel 1 Rot, Gelb
-  setYellow(1, HIGH);
 
   // Ampel 2 Gelb
   setGreen(2, LOW);
@@ -67,28 +79,28 @@ void loop() {
 }
 
 void setGreen(int ampel, int value) {
-	if (ampel == 1) {
-		digitalWrite(GREEN1, value);
-	}
-	else {
-		digitalWrite(GREEN2, value);
-	}
+  if (ampel == 1) {
+    digitalWrite(GREEN1, value);
+  }
+  else {
+    digitalWrite(GREEN2, value);
+  }
 }
 
 void setYellow(int ampel, int value) {
-	if (ampel == 1) {
-		digitalWrite(YELLOW1, value);
-	}
-	else {
-		digitalWrite(YELLOW2, value);
-	}
+  if (ampel == 1) {
+    digitalWrite(YELLOW1, value);
+  }
+  else {
+    digitalWrite(YELLOW2, value);
+  }
 }
 
 void setRed(int ampel, int value) {
-	if (ampel == 1) {
-		digitalWrite(RED1, value);
-	}
-	else {
-		digitalWrite(RED2, value);
-	}
+  if (ampel == 1) {
+    digitalWrite(RED1, value);
+  }
+  else {
+    digitalWrite(RED2, value);
+  }
 }
